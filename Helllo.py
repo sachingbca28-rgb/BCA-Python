687d3fe0c3bf8edbfe460b1e24839c3a8973f13a
sachingbca28-rgb <sachingbca28@ipsacademy.org>
2026-07-04 15:32:34 +0530

    Comments, Escape Sequence & Print in Python

---
+++ Helllo.py
@@ -0,0 +1,24 @@
+# Day 5 - Comments, Escape Sequences & Print in Python
+
+# This is a single-line comment
+print("Hello, Python!")
+
+# Escape Sequence Examples
+print("Hello\nWorld")
+print("Python\tProgramming")
+print("He said, \"Welcome to Python!\"")
+
+# Print with separator
+print("Python", "Java", "C++", sep=" | ")
+
+# Print with custom end
+print("Hello", end=" ")
+print("World")
+
+# Multi-line string
+"""
+This is an example of a multi-line string.
+It can also be used as documentation.
+"""
+
+print("Day 5 completed successfully!")
